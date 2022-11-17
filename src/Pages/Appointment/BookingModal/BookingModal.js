bf00de9598ab2c262cc7b64dc263467c35bcf9f0
zahidhasan125 <zahidhasan125@gmail.com>
2022-11-17 22:10:59 +0600

    module 72 complete

--- src/Pages/Appointment/BookingModal/BookingModal.js
+++ src/Pages/Appointment/BookingModal/BookingModal.js
@@ -1,9 +1,27 @@
 import { format } from 'date-fns';
 import React from 'react';
 
-const BookingModal = ({ treatment, selectedDate }) => {
+const BookingModal = ({ treatment, setTreatment, selectedDate }) => {
     const { name, slots } = treatment;
-    const date = format(selectedDate, 'PP')
+    const appointmentDate = format(selectedDate, 'PP');
+    const handleBooking = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const slot = form.slot.value;
+        const patientName = form.name.value;
+        const email = form.email.value;
+        const phone = form.email.value;
+        const info = {
+            treatment: name,
+            appointmentDate,
+            slot,
+            patientName,
+            email,
+            phone
+        }
+        console.log(info);
+        setTreatment(null);
+    }
     return (
         <>
             <input type="checkbox" id="booking-modal" className="modal-toggle" />
@@ -11,17 +29,19 @@ const BookingModal = ({ treatment, selectedDate }) => {
                 <div className="modal-box relative">
                     <label htmlFor="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <h3 className="text-lg font-bold mb-8">{name}</h3>
-                    <input type="text" value={date} className="input input-bordered input-md w-full mb-6 text-base" disabled />
-                    <select className="select select-bordered select-md w-full mb-6 text-base" defaultValue={'Default'}>
-                        <option disabled value='Default'>Select a time slot</option>
-                        {
-                            slots.map((slot, idx) => <option key={idx} value={slot}>{slot}</option>)
-                        }
-                    </select>
-                    <input type="text" placeholder="Full Name" className="input input-bordered input-md w-full mb-6 text-base" />
-                    <input type="text" placeholder="Phone Number" className="input input-bordered input-md w-full mb-6 text-base" />
-                    <input type="email" placeholder="Email" className="input input-bordered input-md w-full mb-6 text-base" />
-                    <input type="submit" value="Submit" className='btn btn-sm w-full h-12 text-white' />
+                    <form onSubmit={handleBooking}>
+                        <input type="text" value={appointmentDate} className="input input-bordered input-md w-full mb-6 text-base" disabled />
+                        <select name='slot' className="select select-bordered select-md w-full mb-6 text-base" defaultValue={'Default'}>
+                            <option disabled value='Default'>Select a time slot</option>
+                            {
+                                slots.map((slot, idx) => <option key={idx} value={slot}>{slot}</option>)
+                            }
+                        </select>
+                        <input name='name' type="text" placeholder="Full Name" className="input input-bordered input-md w-full mb-6 text-base" />
+                        <input name='email' type="email" placeholder="Email" className="input input-bordered input-md w-full mb-6 text-base" required/>
+                        <input name='phone' type="text" placeholder="Phone Number" className="input input-bordered input-md w-full mb-6 text-base" />
+                        <input type="submit" value="Submit" className='btn btn-sm w-full h-12 text-white' />
+                    </form>
                 </div>
             </div>
         </>
